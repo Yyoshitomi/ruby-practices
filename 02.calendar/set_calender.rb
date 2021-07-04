81517721d58511926daabf942ea4bd91805f9128
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2021-07-04 21:29:55 +0900

    不要な変数を削除

--- 02.calendar/set_calender.rb
+++ 02.calendar/set_calender.rb
@@ -10,8 +10,6 @@ def set_calendar(year, mon)
   # カレンダー上部に表示する日付をなんか良い感じの位置に出力する
   puts output_date.center(18)
 
-  specifed_date = Date.new(year, mon, @tdd)
-
   # 週の初めの曜日
   first_wday = Date.new(year, mon, 1).wday
   # 月末の日付
@@ -29,13 +27,14 @@ def set_calendar(year, mon)
 
   # 初日から月末の日の間に繰り返す感じのeach文
   (1..last_date).each do |date|
-    # (1..last_date)から順番に取り出した値(date)を右詰で出力していく
-    if year == @tdy && mon == @tdm && date == @tdd
+    # 今日の日付を含むカレンダーを表示する場合は反転
+    if (year == @tdy) && (mon == @tdm) && (date == @tdd)
       CE.fg(:black).bg(:white)
     else
       CE.fg(:white).bg(:black)
     end
 
+    # (1..last_date)から順番に取り出した値(date)を右詰で出力していく
     print date.to_s.rjust(2) + " "
 
     # 次の日付を生成しておく
