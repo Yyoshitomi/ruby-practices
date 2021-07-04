3804c8f7426f7ad9252293ff72b700b48390909d
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2021-07-04 21:29:34 +0900

    指定した値が整数であるかどうかをチェックするよう変更

--- 02.calendar/is_valid_date.rb
+++ 02.calendar/is_valid_date.rb
@@ -2,25 +2,22 @@ require_relative './set_date'
 
 def isValid_year(year, mon)
   n_year = year.to_i
-
-  if 9999 >= n_year && n_year >= 1
+  str_year = year.to_s
+  
+  if (str_year =~ /^[0-9]+$/) && (9999 >= n_year) && (n_year >= 1)
     isValid_mon(n_year, mon)
-  elsif n_year == 0
-    print "cal: year `#{year.to_s}' not in range 1..9999\n"
-  else
-    print "cal: year `#{n_year}' anot in range 1..9999\n"
+  elsif
+    print "cal: year `#{str_year}' not in range 1..9999\n"
   end
 end
 
 def isValid_mon(year, mon)
   n_mon = mon.to_i
-  p n_mon
+  str_mon = mon.to_s
 
-  if 12 >= n_mon && n_mon >= 1
+  if (str_mon =~ /^[0-9]+$/) && (12 >= n_mon) && (n_mon >= 1)
     set_calendar(year, n_mon)
-  elsif n_mon == 0
-    print "cal: #{mon.to_s} is neither a month number (1..12) nor a name\n"
-  else
-    print "cal: #{n_mon} is neither a month number (1..12) nor a name\n"
+  elsif
+    print "cal: #{str_mon} is neither a month number (1..12) nor a name\n"
   end
 end
