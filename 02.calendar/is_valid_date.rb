c5adb49bd3b2c262b044e474de04ac28e0d7506a
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2021-07-04 20:30:32 +0900

    指定日の検証を行うメソッドを追加

---
+++ 02.calendar/is_valid_date.rb
@@ -0,0 +1,25 @@
+require_relative './set_date'
+
+def isValid_year(year, mon)
+  n_year = year.to_i
+
+  if 9999 > n_year && n_year > 1
+    isValid_mon(year, mon)
+  elsif n_year == 0
+    print "cal: year `#{year.to_s}' not in range 1..9999\n"
+  else
+    print "cal: year `#{n_year}' anot in range 1..9999\n"
+  end
+end
+
+def isValid_mon(year, mon)
+  n_mon = mon.to_i
+
+  if 13 > n_mon && n_mon > 1
+    set_calendar(year.to_i, mon.to_i)
+  elsif n_mon == 0
+    print "cal: #{mon.to_s} is neither a month number (1..12) nor a name\n"
+  else
+    print "cal: #{n_mon} is neither a month number (1..12) nor a name\n"
+  end
+end
