2d7ef2175cefa09af965cdacf1ce5bb5e059c660
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2021-07-17 21:32:54 +0900

    オプションの指定時の条件分岐修正

--- 02.calendar/is_valid_date.rb
+++ 02.calendar/is_valid_date.rb
@@ -1,24 +1,24 @@
 # 指定された値を検証する
 # 年は1-9999、かつ月は1-12の範囲内の整数であるかどうかを検証
 # 文字列や少数など整数でなさそうな場合などはメッセージを表示する
-def is_valid_year(year, mon)
+def is_valid_year(year)
   n_year = year.to_i
   str_year = year.to_s
   
   if (9999 >= n_year) && (n_year >= 1)
-    isValid_mon(n_year, mon)
-  elsif
+    @opt_year = n_year
+  else
     print "cal: year `#{str_year}' not in range 1..9999\n"
   end
 end
 
-def is_valid_mon(year, mon)
+def is_valid_mon(mon)
   n_mon = mon.to_i
   str_mon = mon.to_s
 
   if (str_mon =~ /^[0-9]+$/) && (12 >= n_mon) && (n_mon >= 1)
-    set_calendar(year, n_mon)
-  elsif
+    @opt_mon = n_mon
+  else
     print "cal: #{str_mon} is neither a month number (1..12) nor a name\n"
   end
 end
