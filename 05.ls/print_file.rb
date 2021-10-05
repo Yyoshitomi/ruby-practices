08e5b39c275d0443f654a297b8d1ec57f916fdf8
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2021-10-05 21:10:18 +0900

    不要なコメントを削除

--- 05.ls/print_file.rb
+++ 05.ls/print_file.rb
@@ -6,13 +6,9 @@ class PrintFile
   def output(files, opts)
     sorted_files = sorted_files(files, opts)
 
-    # ファイル名が一番長いファイル名の長さ確認 & 整形
     max_length = sorted_files.max_by(&:length).length + 2
-    # ファイル数を計算
     count = sorted_files.count
     col = 3
-    # ターミナル画面のサイズを一番長いファイル名で割って列を計算
-    # col_count = `tput cols`.to_i / max_length
     col_count = (`tput cols`.to_i / max_length) > col ? col : (`tput cols`.to_i / max_length)
 
     if col_count >= count
@@ -26,7 +22,6 @@ class PrintFile
   private
 
   def one_row(files, len)
-    # files.each { |f| print f.ljust(len, ' ') }
     space = ' ' * 11
 
     files.each do |f|
@@ -42,19 +37,13 @@ class PrintFile
     count = split_screen[1]
     col_count = split_screen[2]
 
-    # ファイル数を列で割って行数を計算
     row = (count.to_f / col_count).ceil
 
-    # 行頭が[1,4,7,10]にするための配列を作成
     export_files = []
-    # ary.firstが[1,4,7,10]になるようにスライス
     ary_row = files.each_slice(row).to_a
 
     # ary_row内の各配列を先頭から並び替える
     row.times do |n|
-      # ary_row.each do |ary|
-      #   file = ary[n]
-      #   export_files.push(file) unless file.nil? # nilは省く
       col_count.times do |i|
         file = ary_row[i][n]
         export_files.push(file)
@@ -63,8 +52,6 @@ class PrintFile
       end
     end
 
-    # 一行ずつ並べる
-    # rows = export_files.each_slice(ary_row.count).to_a
     rows = export_files.each_slice(col_count).to_a
     rows.each { |line| one_row(line, max_length) }
   end
