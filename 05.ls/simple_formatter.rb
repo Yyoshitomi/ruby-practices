5a4311e2d771e87908eca913b8aa62e0439e09d1
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2022-01-14 13:46:19 +0900

    出力時の処理を変更

--- 05.ls/simple_formatter.rb
+++ 05.ls/simple_formatter.rb
@@ -7,11 +7,10 @@ class SimpleFormatter
 
   def output(files)
     max_length = files.max_by(&:length).length + 2
-    file_count = files.count
     max_size_divider = (IO.console.winsize[1] / max_length)
     col_count = max_size_divider > COL ? COL : max_size_divider
 
-    display_rows(files, max_length, file_count, col_count)
+    display_rows(files, max_length, col_count)
   end
 
   private
@@ -25,28 +24,45 @@ class SimpleFormatter
     print "\n"
   end
 
-  def display_rows(files, max_length, file_count, col_count)
-    row_count = (file_count.to_f / col_count).ceil
-    rows = files.each_slice(row_count).to_a
-    status_display_just_for_line = files.count >= COL && COL > rows[0].size && COL > rows.size
+  def display_rows(files, max_length, col_count)
+    row_count = (files.count.to_f / col_count).ceil
 
-    if status_display_just_for_line
-      file = rows[0].pop
-      exported_files = rows.flatten
-      exported_files.push(file)
+    if row_count == 1
+      exported_files = files
     else
       exported_files = []
-      row_count.times do |row_idx|
-        col_count.times do |col_idx|
-          file = rows[col_idx][row_idx]
-          exported_files.push(file)
 
-          break if rows[col_idx + 1].nil?
-        end
+      cols = make_cols(col_count, files)
+      row_count.times do |row_idx|
+        cols.each { |col| exported_files.push(col[row_idx]) }
       end
     end
 
     file_table = exported_files.each_slice(col_count).to_a
     file_table.each { |line| display_row(line, max_length) }
+  end
+
+  # in_groupsメソッドを参考
+  # https://github.com/rails/rails/blob/fbe2433be6e052a1acac63c7faf287c52ed3c5ba/activesupport/lib/active_support/core_ext/array/grouping.rb#L62-L86
+  def make_cols(col_count, files)
+    division = files.count / col_count
+    modulo = files.count % col_count
+
+    col_array = []
+    start = 0
+    pre_length = 0
+    left_over = 0
+    col_count.times do |i|
+      length = division + (modulo.positive? && modulo > i ? 1 : 0)
+      length += (left_over > pre_length && pre_length > length ? 1 : 0)
+
+      col_array << files.slice(start, length)
+
+      start += length
+      pre_length = length
+      left_over = (files.count - start)
+    end
+
+    col_array
   end
 end
