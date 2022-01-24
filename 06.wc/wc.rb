fb20735027092da96d5c8cbadff3e5c093331d1e
Yyoshitomi <42134813+Yyoshitomi@users.noreply.github.com>
2022-01-24 22:34:30 +0900

    rubocop対応

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -6,56 +6,43 @@ require 'optparse'
 
 def main
   options = {}
-  optparse = OptionParser.new do |opts|
-    opts.on('-l', '--lines', 'count text’s lines') { options[:l] = true }
-  end
+  optparse = OptionParser.new { |opts| opts.on('-l', '--lines', 'count text’s lines') { options[:l] = true } }
 
   optparse.parse!(ARGV)
 
-  if ARGV == []
-    print_count count(readlines.join, options)
-  else
-    display_count_file(ARGV, options)
-  end
+  ARGV == [] ? print_count(count(readlines.join, options)) : display_count_file(ARGV, options)
 end
 
 def display_count_file(argv, options)
-  total_count = []
+  total_count = Array.new(options[:l] ? 1 : 3, 0)
   argv.each do |arg|
     if FileTest.file?(arg)
-      count = count(File.read(arg), options)
-      print_count(count, arg)
-      total_count << count
+      current_count = total_count
+      text_count = count(File.read(arg), options)
+      print_count(text_count, arg)
+      total_count = current_count.zip(text_count).map(&:sum) if argv.count > 1
     else
       print_error_message(arg)
     end
   end
 
-  return if total_count.empty?
-
-  print_count total_count.transpose.map { |c| c.inject(:+) }, 'total'
+  print_count(total_count, 'total') if argv.count > 1
 end
 
 def count(text, options)
-  lines = text.split(/\R/).size
-  words = text.split(/\s+/).size
-  bytes = text.bytesize
+  text_count = [text.split(/\R/).size]
+  text_count += [text.split(/\s+/).size, text.bytesize] unless options[:l]
 
-  options[:l] ? [lines] : [lines, words, bytes]
+  text_count
 end
 
 def print_count(text_count, name = nil)
-  text_count.each { |count| print count.to_s.rjust(8, ' ') }
-  puts " #{name}"
+  puts "#{text_count.map { |c| c.to_s.rjust(8, ' ') }.join} #{name}"
 end
 
 def print_error_message(arg)
-  error_messege = if FileTest.directory?(arg)
-                    'read: Is a directory'
-                  else
-                    'open: No such file or directory'
-                  end
-  puts "wc: #{arg}: #{error_messege}"
+  messege = FileTest.directory?(arg) ? 'read: Is a directory' : 'open: No such file or directory'
+  puts "wc: #{arg}: #{messege}"
 end
 
 main
